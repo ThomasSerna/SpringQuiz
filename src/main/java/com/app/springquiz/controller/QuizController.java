0b59fad0a582b76b0d29e0004e8ed78e0cd17077
Thomas Serna <tasernas@eafit.edu.co>
2025-12-28 16:12:23 -0500

    QuestionsService made and basic login-register endpoints

--- src/main/java/com/app/springquiz/controller/QuizController.java
+++ src/main/java/com/app/springquiz/controller/QuizController.java
@@ -1,4 +1,26 @@
 package com.app.springquiz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class QuizController {
+
+    @GetMapping("/register")
+    public String registerPage(){
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage(){
+        return "logout";
+    }
+
+
+
 }
