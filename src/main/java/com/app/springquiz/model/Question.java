c4215a51f76bce1b2bfb23c828621858f0f73c26
Thomas Serna <tasernas@eafit.edu.co>
2025-12-28 15:40:23 -0500

    models made

---
+++ src/main/java/com/app/springquiz/model/Question.java
@@ -0,0 +1,62 @@
+package com.app.springquiz.model;
+
+import java.util.ArrayList;
+
+public class Question {
+
+    private int id;
+    private String questionText;
+    private ArrayList<String> options = new ArrayList<>();
+    private String correctAnswer;
+
+    public Question() {}
+
+    public Question(int id, String questionText, ArrayList<String> options, String correctAnswer) {
+        this.id = id;
+        this.questionText = questionText;
+        this.options = options != null ? new ArrayList<>(options) : new ArrayList<>();
+        this.correctAnswer = correctAnswer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public ArrayList<String> getOptions() {
+        return new ArrayList<>(options);
+    }
+
+    public void setOptions(ArrayList<String> options) {
+        this.options = options != null ? new ArrayList<>(options) : new ArrayList<>();
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", questionText='" + questionText + '\'' +
+                ", options=" + options +
+                ", correctAnswer='" + correctAnswer + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/app/springquiz/model/User.java
+++ src/main/java/com/app/springquiz/model/User.java
@@ -1,4 +1,66 @@
 package com.app.springquiz.model;
 
 public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    public User() {}
+
+    public User(String username, String email, String password, String role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // password intentionally has a getter and setter, but is ignored in JSON serialization
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
